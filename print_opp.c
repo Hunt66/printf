ac32e26d5f62a25f8e36b6c29cd0033db32e8bd6
Hunt6666 <489@holbertonschool.com>
2018-07-28 03:06:07 +0000

    This is what I have so far for first three I am testing to see if we need to bother with incorect input

--- print_opp.c
+++ print_opp.c
@@ -1,1 +1,55 @@
 #include"holberton.h"
+#include <unistd.h>
+
+/**
+ *_printfc - prints an input char
+ *@args: the input char
+ *Return: void
+ */
+
+
+int _printfc(va_list args)
+{
+	_putchar((char)va_arg(args, int));
+}
+
+
+/**
+ *_printfs- prints an input string
+ *@args: input string
+ *Return: void
+ */
+
+
+int _printfs(va_list args)
+{
+	int i;
+	char c = 'a';
+	/*make test for is string maby or maby not
+	 I am testing with printfd and will test later*/
+	while (1)
+	{
+		c = (char)va_arg(args, int);
+		if (c == '\0')
+			break;
+		_putchar(c);
+	}
+	return (0);
+}
+
+
+/**
+ *_printfp- prints % char
+ */
+
+
+void _printfp(void)
+{
+	_putchar('%');
+}
+
+
+
+
+/**
+ *_printfi - prin
