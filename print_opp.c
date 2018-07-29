5ce0c505026630cb184ac693db26e43da886b96c
Hunt6666 <489@holbertonschool.com>
2018-07-29 21:52:32 +0000

    alright the first three are working but type checks not in place

--- print_opp.c
+++ print_opp.c
@@ -1,38 +1,45 @@
 #include"holberton.h"
 #include <unistd.h>
 #include <stdarg.h>
+
+#include"test_header.h"
+
+
 /**
  *_printfc - prints an input char
  *@args: the input char
- *Return: void
+ *Return: 0 if does not fail
  */
 
 
 int _printfc(va_list args)
 {
 	_putchar((char)va_arg(args, int));
+	return (0);
 }
 
 
 /**
  *_printfs- prints an input string
  *@args: input string
- *Return: void
+ *Return: 0 if does not fail
  */
 
 
 int _printfs(va_list args)
 {
 	int i;
-	char c = 'a';
-	/*make test for is string maby or maby not
-	 I am testing with printfd and will test later*/
-	while (1)
+	char *str = (char *)va_arg(args, char *);
+
+	/*I get a gcc error when I run printf on an int and it is
+	 also true when I do it on strlen so this might work*/
+	/*if (!strlen(str))
+	  return (-1);*/
+
+
+	for (i = 0 ; str[i] ; i++)
 	{
-		c = (char)va_arg(args, int);
-		if (c == '\0')
-			break;
-		_putchar(c);
+		_putchar(str[i]);
 	}
 	return (0);
 }
@@ -47,9 +54,3 @@ void _printfp(void)
 {
 	_putchar('%');
 }
-
-
-
-
-/**
- *_printfi - prin
