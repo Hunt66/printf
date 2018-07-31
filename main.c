d0f6b3bf2ee29244b6d4cd0649eefc412e033487
Hunt6666 <489@holbertonschool.com>
2018-07-31 15:24:28 -0700

    Adds changes

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +9,13 @@
  */
 int main(void)
 {
-    _printf("%b\n", 98);
+	unsigned int ui;
+
+	  ui = (unsigned int)INT_MAX +1024;
+	  _printf("ui is: %d\n", ui);
+	  /*_printf("%b\n", 98);*/
+	  printf("testing negative with real: %u\n", -10284);
+	  _printf("testing negative: %b\n", 0);
+     _printf("testing ui: %b\n", ui);
     return (0);
 }
